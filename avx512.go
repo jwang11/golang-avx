9b63ef6ee6d61ad6485c0537fdcf095845a5ea02
jwang <jing.j.wang@intel.com>
2019-11-20 05:54:14 +0000

    clean code and add bench

--- avx512.go
+++ avx512.go
@@ -8,57 +8,6 @@ package avx512
 #include <stdlib.h>
 #include <x86intrin.h>
 #include <immintrin.h>
-void avx_add(const size_t n, float *x, float *y, float *z)
-{
-    static const size_t single_size = 16;
-    const size_t end = n / single_size;
-    __m512 *vz = (__m512 *)z;
-    __m512 *vx = (__m512 *)x;
-    __m512 *vy = (__m512 *)y;
-    for(size_t i=0; i<end; ++i) {
-      vz[i] = _mm512_add_ps(vx[i], vy[i]);
-    }
-}
-
-void avx_sub(const size_t n, float *x, float *y, float *z)
-{
-    static const size_t single_size = 16;
-    const size_t end = n / single_size;
-    __m512 *vz = (__m512 *)z;
-    __m512 *vx = (__m512 *)x;
-    __m512 *vy = (__m512 *)y;
-    for(size_t i=0; i<end; ++i) {
-      vz[i] = _mm512_sub_ps(vx[i], vy[i]);
-    }
-}
-
-void avx_mul(const size_t n, float *x, float *y, float *z)
-{
-    static const size_t single_size = 16;
-    const size_t end = n / single_size;
-    __m512 *vz = (__m512 *)z;
-    __m512 *vx = (__m512 *)x;
-    __m512 *vy = (__m512 *)y;
-    for(size_t i=0; i<end; ++i) {
-      vz[i] = _mm512_mul_ps(vx[i], vy[i]);
-    }
-}
-
-float avx_dot(const size_t n, float *x, float *y)
-{
-    static const size_t single_size = 8;
-    const size_t end = n / single_size;
-    __m512 *vx = (__m512 *)x;
-    __m512 *vy = (__m512 *)y;
-    __m512 vsum = {0};
-    for(size_t i=0; i<end; ++i) {
-      vsum = _mm512_add_ps(vsum, _mm512_mul_ps(vx[i], vy[i]));
-    }
-    __attribute__((aligned(32))) float t[16] = {0};
-    _mm512_store_ps(t, vsum);
-    return t[0] + t[1] + t[2] + t[3] + t[4] + t[5] + t[6] + t[7] +
-	t[8] + t[9] + t[10] + t[11] + t[12] + t[13] + t[14] + t[15];
-}
 
 int32_t avx_dot_vnni(const size_t n, int8_t *x, int8_t *y)
 {
@@ -73,38 +22,40 @@ int32_t avx_dot_vnni(const size_t n, int8_t *x, int8_t *y)
     int32_t *t = (int32_t *)&vsum;
     volatile int32_t sum = 0;
     for (int i=0; i<16; i++) {
-      printf("%d ", t[i]);
+      sum += t[i];
+    }
+    return sum;
+
+}
+
+int32_t avx_dot_int8(const size_t n, int8_t *x, int8_t *y)
+{
+    static const size_t single_size = 64;
+    const size_t end = n / single_size;
+    const int16_t op4[32] = {[0 ... 31] = 1};
+    __m512i *vx = (__m512i *)x;
+    __m512i *vy = (__m512i *)y;
+    volatile __m512i vsum = {0};
+    for(size_t i=0; i<end; ++i) {
+      __m512i vresult1 = _mm512_maddubs_epi16(vx[i], vy[i]);
+      __m512i vresult2 = _mm512_madd_epi16(vresult1, *(__m512i *)&op4);
+      vsum = _mm512_add_epi32(vsum, vresult2);
+    }
+    int32_t *t = (int32_t *)&vsum;
+    volatile int32_t sum = 0;
+    for (int i=0; i<16; i++) {
       sum += t[i];
     }
     return sum;
 
 }
 */
-
 import "C"
 import (
 	"math"
 	"reflect"
 	"unsafe"
 )
-
-func MmMalloc(size int) []float32 {
-	size_ := size
-	size = align(size)
-	ptr := C._mm_malloc((C.size_t)(C.sizeof_float*size), 64)
-	hdr := reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(ptr)),
-		Len:  size,
-		Cap:  size,
-	}
-	goSlice := *(*[]float32)(unsafe.Pointer(&hdr))
-	if size_ != size {
-		for i := size_; i < size; i++ {
-			goSlice[i] = 0.0
-		}
-	}
-	return goSlice
-}
 
 func MmMalloc_int8(size int) []int8 {
 	size_ := size
@@ -124,38 +75,19 @@ func MmMalloc_int8(size int) []int8 {
 	return goSlice
 }
 
-func MmFree(v []float32) {
-	C._mm_free(unsafe.Pointer(&v[0]))
-}
-
 func MmFree_int8(v []int8) {
 	C._mm_free(unsafe.Pointer(&v[0]))
-}
-
-func Add(size int, x, y, z []float32) {
-	size = align(size)
-	C.avx_add((C.size_t)(size), (*C.float)(&x[0]), (*C.float)(&y[0]), (*C.float)(&z[0]))
-}
-
-func Mul(size int, x, y, z []float32) {
-	size = align(size)
-	C.avx_mul((C.size_t)(size), (*C.float)(&x[0]), (*C.float)(&y[0]), (*C.float)(&z[0]))
-}
-
-func Sub(size int, x, y, z []float32) {
-	size = align(size)
-	C.avx_sub((C.size_t)(size), (*C.float)(&x[0]), (*C.float)(&y[0]), (*C.float)(&z[0]))
-}
-
-func Dot(size int, x, y []float32) float32 {
-	size = align(size)
-	dot := C.avx_dot((C.size_t)(size), (*C.float)(&x[0]), (*C.float)(&y[0]))
-	return float32(dot)
 }
 
 func Dot_vnni(size int, x, y []int8) int32 {
 	size = align(size)
 	dot := C.avx_dot_vnni((C.size_t)(size), (*C.int8_t)(&x[0]), (*C.int8_t)(&y[0]))
+	return int32(dot)
+}
+
+func Dot_int8(size int, x, y []int8) int32 {
+	size = align(size)
+	dot := C.avx_dot_int8((C.size_t)(size), (*C.int8_t)(&x[0]), (*C.int8_t)(&y[0]))
 	return int32(dot)
 }
 
--- avx512_test.go
+++ avx512_test.go
@@ -4,80 +4,24 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	for _, size := range []int{16, 32, 64} {
+func TestDot_int8(t *testing.T) {
+	for _, size := range []int{63, 64, 127, 128, 256} {
 		func(size int) {
-			x := MmMalloc(size)
-			y := MmMalloc(size)
-			z := MmMalloc(size)
-			defer MmFree(x)
-			defer MmFree(y)
-			defer MmFree(z)
+			x := MmMalloc_int8(size)
+			y := MmMalloc_int8(size)
+			defer MmFree_int8(x)
+			defer MmFree_int8(y)
 
-			truth := make([]float32, size)
+			var truth int32
 			for i := 0; i < size; i++ {
-				x[i] = float32(i)
-				y[i] = float32(i + 1)
-				truth[i] = x[i] + y[i]
+				x[i] = int8(i % 127)
+				y[i] = int8((i + 1) % 127)
+				truth += int32(x[i]) * int32(y[i])
 			}
 
-			Add(size, x, y, z)
-
-			for i := 0; i < size; i++ {
-				if truth[i] != z[i] {
-					t.Errorf("Add should return %f in %d, but %f", truth[i], i, z[i])
-				}
-			}
-		}(size)
-	}
-}
-
-func TestSub(t *testing.T) {
-	for _, size := range []int{16, 32, 64} {
-		func(size int) {
-			x := MmMalloc(size)
-			y := MmMalloc(size)
-			z := MmMalloc(size)
-			defer MmFree(x)
-			defer MmFree(y)
-			defer MmFree(z)
-
-			truth := make([]float32, size)
-			for i := 0; i < size; i++ {
-				x[i] = float32(i)
-				y[i] = float32(i + 1)
-				truth[i] = x[i] - y[i]
-			}
-
-			Sub(size, x, y, z)
-
-			for i := 0; i < size; i++ {
-				if truth[i] != z[i] {
-					t.Errorf("Mul should return %f in %d, but %f", truth[i], i, z[i])
-				}
-			}
-		}(size)
-	}
-}
-
-func TestDot(t *testing.T) {
-	for _, size := range []int{15, 31, 64} {
-		func(size int) {
-			x := MmMalloc(size)
-			y := MmMalloc(size)
-			defer MmFree(x)
-			defer MmFree(y)
-
-			var truth float32
-			for i := 0; i < size; i++ {
-				x[i] = float32(i)
-				y[i] = float32(i + 1)
-				truth += x[i] * y[i]
-			}
-
-			result := Dot(size, x, y)
+			result := Dot_int8(size, x, y)
 			if truth != result {
-				t.Errorf("Dot should return %f, but %f", truth, result)
+				t.Errorf("Dot should return %d, but %d", truth, result)
 			}
 		}(size)
 	}
@@ -104,4 +48,42 @@ func TestDot_vnni(t *testing.T) {
 			}
 		}(size)
 	}
+}
+
+func BenchmarkAVX512DotVnni(b *testing.B) {
+    size := 1024
+	
+    x := MmMalloc_int8(size)
+    y := MmMalloc_int8(size)
+    defer MmFree_int8(x)
+    defer MmFree_int8(y)
+    for i := 0; i < size; i++ {
+        x[i] = int8(i % 127)
+        y[i] = int8((i + 1) % 127)
+    }
+	b.SetBytes(int64(size))
+    b.ResetTimer()
+	var result int32
+    for i := 0; i < b.N; i++ {
+		result += Dot_vnni(size, x, y)
+    }
+}
+
+func BenchmarkAVX512DotInt8(b *testing.B) {
+    size := 1024
+	
+    x := MmMalloc_int8(size)
+    y := MmMalloc_int8(size)
+    defer MmFree_int8(x)
+    defer MmFree_int8(y)
+    for i := 0; i < size; i++ {
+        x[i] = int8(i % 127)
+        y[i] = int8((i + 1) % 127)
+    }
+	b.SetBytes(int64(size))
+    b.ResetTimer()
+	var result int32
+    for i := 0; i < b.N; i++ {
+		result += Dot_int8(size, x, y)
+    }
 }
